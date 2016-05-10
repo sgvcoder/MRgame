88866d8ced5039f1898c53628e9910140cf990d2
Serg G <sgvcoder@gmail.com>
2016-05-10 19:39:07 +0300

    dashboard

--- dev/socket.js
+++ dev/socket.js
@@ -145,6 +145,6 @@ module.exports = function(io) {
 var socketsData = {};
 
 
-var interval = setInterval(function(str1, str2) {
-  console.log(str1 + " " + str2);
-}, 1000, "Hello.", "How are you?");
+// var interval = setInterval(function(str1, str2) {
+//   console.log(str1 + " " + str2);
+// }, 1000, "Hello.", "How are you?");
